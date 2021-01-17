1a9ffd786bf4d44caaa1e36e947cd2f53a165bb3
wangfpp <wangfpp@gmail.com>
2021-01-17 16:28:28 +0800

    实现Promise

--- Promise/asset/js/index.js
+++ Promise/asset/js/index.js
@@ -1,8 +1,44 @@
-new ZkPromise((resolve, reject) => {
-    resolve("aaaa");
-    // reject(aaa)
-}).then(res => {
-    console.log("Promise Resolve:" + res);
+let p = new ZkPromise((resolve, reject) => {
+        // reject(1111);
+        setTimeout(() => {
+            resolve(111);
+        }, 2000)
+})
+.then(
+    res => {
+        let p2 = p.then(res => {
+            return p2;
+        })
+    },
+    err => {
+        console.log(aaa);
+        
+    }
+)
+.then(res => {
+    console.log("Second promise resolve:" + res);
+}, err => {
+    console.log("Second promise reject:" + err);
 })
 
-// console.log(p);
+console.log(p);
+
+
+
+// let p2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(111);
+//     }, 2000);
+// }).then(res => {
+//     console.log("原生promise正确回调:" + res);
+//     let p3 = p2.then(res => {
+//         return p3;
+//     })
+// }, err => {
+//     console.log("原生promise错误回调:" + err);
+//     // return new Promise()
+// }).then(res => {
+//     console.log("第二层 reslve:" + res);
+// }, err => {
+//     console.log("第二层rekject" + err);
+// })
--- Promise/asset/js/promise.js
+++ Promise/asset/js/promise.js
@@ -10,6 +10,7 @@ class ZkPromise {
         this.status = ZkPromise.PENDING;
         this.value = null;
         this.reason = null;
+        this.pendingTask = [];
         // try为了捕获在Promise内的代码错误捕获到错误后直接执行reject
         try {
             exector(this.resolve.bind(this), this.reject.bind(this));
@@ -22,6 +23,12 @@ class ZkPromise {
         if (this.status === ZkPromise.PENDING) {
             this.status = ZkPromise.FULFILLED;
             this.value = value;
+            this.pendingTask.forEach(task => {
+                let { onFulfilled } = task;
+                setTimeout(() => {
+                    onFulfilled(value);
+                })
+            })
         }
     }
 
@@ -29,24 +36,135 @@ class ZkPromise {
         if (this.status === ZkPromise.PENDING) {
             this.status = ZkPromise.REJECTED;
             this.value = reason;
+            this.pendingTask.forEach(task => {
+                let { onRejected } = task;
+                setTimeout(() => {
+                    onRejected(reason);
+                })
+            })
         }
     }
+
     then(onFulfilled, onRejected) {
-        console.log(this);
-       
         // 以下的两个判断解决 then内的方法可以不传
         // 重新学习一下typeof !!!!!
         if (typeof onFulfilled  !== "function"){
-            onFulfilled = this.noop
+            onFulfilled = () => {
+                return this.value
+            };
+            // 返回this.value 解决then的穿透 可以组成往下传递上层的值
         }
         if (typeof onRejected  !== "function"){
-            onRejected = this.noop
+            onRejected = () => this.value;
         }
-        if (this.status === ZkPromise.FULFILLED) {
-            onFulfilled(this.value);
+        // 这里加一个返回Promise是为了解决链式调用的返回
+        let resultPromise = new ZkPromise((resolve, reject) => {
+            // Pending 时有异步任务需要把任务存储 在resolve和reject中再去执行具体任务
+            if (this.status === ZkPromise.PENDING) {
+                this.pendingTask.push({
+                    onFulfilled: value => {
+                        try{
+                            let result = onFulfilled(value);
+                            this.resolveResult(resultPromise, result, resolve, reject);
+                        } catch(err) {
+                            reject(err);
+                        }
+                        // try{
+                        //     let result = onFulfilled(value);
+                        //     if (result instanceof ZkPromise) {
+                        //         result.then(resolve, reject)
+                        //     } else {
+                        //         resolve(result);
+                        //     }
+                        // }catch(err) {
+                        //     reject(err);
+                        // }
+                    },
+                    onRejected: reason => {
+                        try{
+                            let result = onRejected(reason);
+                            this.resolveResult(resultPromise, result, resolve, reject);
+                        } catch(err) {
+                            reject(err);
+                        }
+                        // try {
+                        //     let result = onRejected(reason);
+                        //     resolve(result);
+                        // } catch(err) {
+                        //     reject(err);
+                        // }
+                    }
+                })
+            }
+
+            // 要解决onFulfilled 和onRejected中的代码错误捕获
+            // setTimeout 解决异步事件输出
+            if (this.status === ZkPromise.FULFILLED) {
+                setTimeout(() => {
+                    try{
+                        let result = onFulfilled(this.value);
+                        this.resolveResult(resultPromise, result, resolve, reject);
+                    } catch(err) {
+                        reject(err);
+                    }
+                    // try {
+                    //     let result = onFulfilled(this.value);
+                    //     if (result instanceof ZkPromise) {
+                    //         result.then(resolve, reject); // 这里的.then肯定会执行
+                    //     } else {
+                    //         resolve(result);
+                    //     }
+                    // } catch(err) {
+                    //     reject(err);
+                    // }
+                })
+            }
+            if(this.status === ZkPromise.REJECTED) {
+                setTimeout(() => {
+                    try{
+                        let result = onRejected(reason);
+                        this.resolveResult(resultPromise, result, resolve, reject);
+                    } catch(err) {
+                        reject(err);
+                    }
+                    
+                    // try {
+                    //     let result = onRejected(this.value);
+                    //     if (result instanceof ZkPromise) {
+                    //         result.then(res => {
+                    //             resolve(res);
+                    //         }, err => {
+                    //             reject(err);
+                    //         })
+                    //     } else {
+                    //         resolve(result);
+                    //     }
+                    //     // 上一个Promise不管是resolve还是reject只要有返回值则进入第二个Promise的resolve
+                    // } catch (err) {
+                    //     reject(err);
+                    // }
+                })
+            }
+        })
+        return resultPromise;
+    }
+    resolveResult(originPromise, result, resolve, reject) {
+        if (originPromise === result) {
+            throw TypeError("Chaining cycle detected for promise #<Promise>");
         }
-        if(this.status === ZkPromise.REJECTED) {
-            onRejected(this.value);
+        try {
+            if (result instanceof ZkPromise) {
+                result.then(res => {
+                    resolve(res);
+                }, err => {
+                    reject(err);
+                })
+            } else {
+                resolve(result);
+            }
+            // 上一个Promise不管是resolve还是reject只要有返回值则进入第二个Promise的resolve
+        } catch (err) {
+            reject(err);
         }
     }
     noop() {}
