5867c34262c98cd70e934885b4dfe01f06c51652
wangfpp <wangfpp@gmail.com>
2021-01-19 09:45:20 +0800

    changed: bind

--- bind/index.js
+++ bind/index.js
@@ -1,14 +1,17 @@
 if(!Function.prototype.mybind) {
     Function.prototype.mybind = function() {
-        var slice = Array.prototype.slice;
-        var args = slice.call(arguments, 1);
-        var curr_fn = this;
-        var bind_this = arguments[0];
+        /**
+         * 1.　拿到当前的执行函数
+         * 2.　拿到需要绑定的this
+         * 3. 　拿到参数列表
+         */
+        var args = Array.prototype.slice.call(arguments);
+        var bind_this = args.shift();
+        var curr_this = this;
         return function() {
-            console.log(arguments);
             var concat_args = args.concat(Object.values(arguments));
-            console.log(concat_args);
-            return curr_fn.apply(bind_this, concat_args);
+            // 这里加return函数可能有返回值
+            return curr_this.apply(bind_this, concat_args);
         }
     }
 }
@@ -17,10 +20,13 @@ if(!Function.prototype.mybind) {
 var obj = {
     a: 1,
     sayname: function(val) {
-        console.log(this.a);
-        console.log(val);
+        return function(name) {
+            console.log(val, name);
+            console.log(`my name is :${name}, i have number: ${val}`)
+        }
     }
 }
 
 var fn = obj.sayname
-fn.mybind(obj, "222", 333)(100, 200)
+var result = fn.mybind(obj, "222")()("wangfpp");
+console.log(result);
