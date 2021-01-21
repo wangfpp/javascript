0b7a0a346eaa096e3049d8d96396fafa85caeb95
wangfpp <wangfpp@gmail.com>
2021-01-21 13:51:06 +0800

    add: 添加null_undefined

--- new/index.js
+++ new/index.js
@@ -2,21 +2,28 @@ function Person(name, age, sex) {
     this.name = name;
     this.age = age;
     this.sex = sex;
-    return {
-        a: 1
-    }
+    return new String("aaa");
 }
 
 
 let person1 = mynew(Person, 1, 2, 3);
+let person2 = new Person(1, 2, 3);
 console.log(person1);
-
+console.log(person2);
 
 function mynew() {
     let args = Array.prototype.slice.call(arguments)
     let originFn = args.shift();
     let o = {};
     o.__proto__ = originFn.prototype;
-    let result = originFn.apply(o, ...arguments);
+    let result = originFn.apply(o, args);
     return result instanceof Object ? result : o;
+}
+
+if(true) {
+    tmp = "abc";
+    console.log(tmp);
+    let tmp;
+    console.log(tmp);
+    
 }
---
+++ null_undefined/index.js
@@ -0,0 +1,21 @@
+/**
+ * undefined 表示该有而没有的值 
+ * 因为该有而没有程序也不知道到底该为什么值所有Number(undefined) => NaN
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *  */ 
+
+
+ /*
+  *null 这表示此处就不该有值 
+  * 程序是很明确这一情况的 所以可以直接Number(null) => 0
+  * 
+  * 
+  * 
+  */
+
+  // http://www.ruanyifeng.com/blog/2014/03/undefined-vs-null.html
